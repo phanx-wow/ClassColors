eb5e048cb5269f0bdcc61676c6736763f8303c17
Akkorian-257529 <Akkorian-257529@be1c17f7-4e94-4cd0-bcec-9ec1f3a6e6b2>
2011-01-17 23:55:38 +0000

    Year 2011

--- trunk/ClassColorsBlizz.lua
+++ trunk/ClassColorsBlizz.lua
@@ -1,8 +1,9 @@
 --[[--------------------------------------------------------------------
 	!ClassColors
-	Provides a non-tainting method for changing raid class colors.
-	by Phanx < addons@phanx.net >
-	Copyright © 2009–2010 Phanx. Some rights reserved. See LICENSE.txt for details.
+	Change class colors without tainting the Blizzard UI.
+	Written by Phanx <addons@phanx.net>
+	Maintained by Akkorian <akkorian@hotmail.com>
+	Copyright © 2009–2011 Phanx. Some rights reserved. See LICENSE.txt for details.
 	http://www.wowinterface.com/downloads/info12513-ClassColors.html
 	http://wow.curse.com/downloads/wow-addons/details/classcolors.aspx
 ----------------------------------------------------------------------]]
